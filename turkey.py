09d428ebe8fa8e71a68309e2da7c63be14aa30d7
joliver06 <146748556+joliver06@users.noreply.github.com>
2023-12-11 11:06:42 -0600

    Create turkey.py

---
+++ turkey.py
@@ -0,0 +1,36 @@
+weight = 0
+
+
+print("Hey! Welcome to the Turkey Cooking Time Calculator!")
+
+
+while True:
+    weight_input = input("How heavy is your turkey in pounds: ")
+
+
+    if weight_input.isdigit():
+        weight = float(weight_input)
+        if weight > 0:
+            break
+        else:
+            print("Enter a valid input. The weight should be greater than 0.")
+    else:
+        print("Oops! That doesn't look like a number. Please enter the weight as a number.")
+
+
+cook_time = weight * 15
+
+
+print("For a " + str(weight) + "-pound turkey, it should take about " + str(cook_time) + " minutes to cook.")
+
+
+
+
+
+
+
+
+
+
+
+
